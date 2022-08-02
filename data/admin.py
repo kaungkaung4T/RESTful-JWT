851b5eeb785468df02786e7e9e75a1b33e42877b
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-02 17:34:44 +0630

    update

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from data.models import Item
 
 # Register your models here.
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "description", "price", "time"]
+
+
+admin.site.register(Item, ItemAdmin)
--- data/models.py
+++ data/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=225)
+    description = models.TextField(max_length=1020)
+    price = models.FloatField()
+    time = models.DateTimeField(auto_now_add=True)
