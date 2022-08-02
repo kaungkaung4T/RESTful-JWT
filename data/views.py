17c1fe36b02cef176ca71c0962096593800651f7
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-03 02:57:23 +0630

    update

--- data/views.py
+++ data/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from data.form import ItemForm
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 
 
@@ -9,8 +11,50 @@ def index(request):
 
 
 def registration(request):
+    if request.method == "POST"
+        user = request.POST["username"]
+        email = request.POST["email"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 == password2:
+            if User.objects.filter(username=user).exists():
+                messages.info(request, "Username already exist")
+                return redirect("registration")
+
+            if User.objects.filter(email=email).exists():
+                messages.info(request, "Email already exist")
+                return redirect("registration")
+
+            User.objects.create_user(username=user, email=email, password=password1)
+            messages.success(request, "Account created")
+            return redirect("login")
+
+        else:
+            messages.info(request, "Passwords are not same")
+            return redirect("registration")
+
+
     return render(request, "registration.html")
 
 
 def login(request):
+    if request.method == "POST":
+        user = request.POST["username"]
+        password1 = request.POST["password"]
+
+        u = auth.authenticate(username=user, password=password1)
+
+        if u:
+            auth.login(request, u)
+            return redirect("/")
+
+        messages.info(request, "Couldn't find your account")
+        return redirect("login")
+
     return render(request, "login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
