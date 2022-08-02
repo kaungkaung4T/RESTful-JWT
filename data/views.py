1228971637732100731a8f16caef2695d34393dd
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-03 02:07:50 +0630

    update

--- data/views.py
+++ data/views.py
@@ -6,3 +6,11 @@ from data.form import ItemForm
 def index(request):
     i = ItemForm()
     return render(request, "index.html", {"i":i})
+
+
+def registration(request):
+    return render(request, "registration.html")
+
+
+def login(request):
+    return render(request, "login.html")
