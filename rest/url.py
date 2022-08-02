325683e94f534c03f0a53bb58e184b6c67a7f0ad
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-02 23:48:05 +0630

    updateJWT

--- rest/url.py
+++ rest/url.py
@@ -1,8 +1,16 @@
 from django.urls import path, include
 from rest import views
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path("api", views.getRoutes),
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
     # POSTMAN
     path("resting", views.Resting.as_view(), name="resting"),
