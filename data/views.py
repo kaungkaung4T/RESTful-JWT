67a8d14c5df8bc9acc3b71af617786b2bcaf93dd
kaungkaung4T <mrkaungminnkhant@gmail.com>
2022-08-02 17:40:37 +0630

    update

--- data/views.py
+++ data/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from data.form import ItemForm
 # Create your views here.
 
 
 def index(request):
-    return render(request, "index.html")
+    i = ItemForm()
+    return render(request, "index.html", {"i":i})
